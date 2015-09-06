93dbce635cfdd5ddb5f2325455dcd782d24f5612
Radu Nicolae <ranicolae@gmail.com>
2015-09-05 20:58:45 -0400

    better design. but still not sure if specifying own callbacks is possible

--- game_sdk/kiboto_game.js
+++ game_sdk/kiboto_game.js
@@ -1,13 +1,61 @@
-function kibotoMessages() {
+/*
+
+	Usage:
+
+		workflow 1 - supplying callbacks:
+
+		...
+		game = new KibotoGame(hostname, port, 123, 000, "bob");
+		game.connect();
+
+		...
+
+		game.event({...}, function (err) {
+				this.responseText;
+				this.statusText;
+			},
+
+			function (err) {
+				this.statusText;
+			});
+
+		workflow 2 - using the global message queue
+
+		...
+		botMessages = new KibotoBotMessages();
+		game = new KibotoGame(hostname, port, 123, 000, "bob");
+		game.connect();
+
+		...
+
+		game.event({...});
+
+		// here, gameLogic is assumed to exist
+		gameLogic.process(botMessages.get());
+
+		botMessages.clear();
+
+		// repeat
+
+
+*/
+
+function KibotoBotMessages() {
 	// store responses in global state because
 	// callback can't access anything in KibotoGame
 	// object
-	// NOTE: this should be a singleton
+	// NOTE: this should be a singleton but can't really enforce it!
 
 	window.kibotoResponses = [];
+	window.messagesToProcess = false;
 
-	this.getResponses = function() {
-		window.kibotoResponses.pop();
+	this.get = function() {
+		return window.kibotoResponses;
+	};
+
+	this.clear = function() {
+		window.kibotoResponses = [];
+		window.messagesToProcess = false;
 	};
 }
 
@@ -25,7 +73,7 @@ function KibotoGame(hostname, port, game_id, session_id, player_id) {
 		// initializes the session
 	};
 
-	this.gameEvent = function(data, callback) {
+	this.event = function(data, callback, errorCallback) {
 		// send an event to kiboto server with
 		// what ever data the game developer wants.
 		// should represent state changes for the current player
@@ -37,19 +85,43 @@ function KibotoGame(hostname, port, game_id, session_id, player_id) {
 
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", "/event", true);
+
+		// handle success
 		if (callback == null) {
 			xhr.onload = function (err) {
 				if (xhr.readyState === 4) {
 					if (xhr.status === 200) {
 						window.kibotoResponses.push(xhr.responseText);
+						window.messagesToProcess = true;
 					} else {
-						// not really sure how to handle errors here...
+						window.kibotoResponses.push(xhr.statusText);
 					}
 				}
 			};
 		} else {
-			xhr.onload = callback;
+			// wrap the function to notify the bot message
+			// object that there are messages to process
+			xhr.onload = function (err) {
+				if (xhr.status == 200 ) {
+					window.messagesToProcess = true;
+					callback("");
+				} else {
+					callback(xhr.statusText);
+				}
+			}
 		}
+
+		// handle errors
+		if (errorCallback == null) {
+			xhr.onerror = function (err) {
+				// should we store text or HTTP codes? or our own mapping?
+				window.kibotoResponses.push(xhr.statusText);
+			}
+		} else {
+			xhr.onerror = errorCallback;
+		}
+
+		xhr.send();
  	};
 }
 
