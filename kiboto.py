20d7fcf4e5cea43424ecc382a22bf4811d01572e
Radu Nicolae <ranicolae@gmail.com>
2015-09-12 14:03:45 -0400

    subscription handler now uses syncronous redis call to avoid overwriting sessions

--- kiboto.py
+++ kiboto.py
@@ -5,6 +5,9 @@ import tornado.web
 from tornado.netutil import bind_sockets
 from tornado.process import fork_processes
 
+# cache
+import redis
+
 # helpers
 import json
 
@@ -15,6 +18,17 @@ from lib.handlers.SessionBroadcastHandler import SessionBroadcastHandler
 from lib.handlers.EventHandler import EventHandler
 from lib.support_systems import session_state
 
+def splash_screen():
+	print "KIBOTO ONLINE"
+	print " _____"
+	print "|  _  |"
+	print "| |_| |"
+	print "| ___ |"
+	print "| ___ |"
+	print "| ___ |"
+	print "|_____|"
+	print
+
 if __name__ == "__main__":
 	# load config files
 	with open('conf/server.config') as f:
@@ -23,16 +37,23 @@ if __name__ == "__main__":
 	# keep a global copy of session cache for speed
 	local_sessions = session_state.SessionHandler()
 
+	# have non-async bot registration so bots can't un-register eachother
+	# see BotSubscriptionHandler implementation for more details
+	sync_redis = redis.StrictRedis() # only local for now
+
 	# multiple request handlers, sharing the same state
 	tornado_app_config = tornado.web.Application([
 			(r"/session", GameSessionInitializer, dict(local_sessions=local_sessions)),
-			(r"/subscribe", BotSubscriptionHandler),
+			(r"/subscribe", BotSubscriptionHandler, dict(sync_redis=sync_redis)),
 			(r"/get_sessions", SessionBroadcastHandler),
 			(r"/event", EventHandler, dict(local_sessions=local_sessions))
 		])
 
 	# listen on the configured port, default to 8888 if not specified
 	sockets = bind_sockets(config.get('port', 8888))
+
+	# :)
+	splash_screen()
 
 	# multi-process tornado. auto forks for every core you have
 	fork_processes(None)
--- lib/handlers/BotSubscriptionHandler.py
+++ lib/handlers/BotSubscriptionHandler.py
@@ -1,31 +1,39 @@
 
 """
+
+	Bot Subscription handler
+
+	Handles requests to subscribe to an inactive session.
+
 	The bots will find the session and player they want to be,
 	then they will send their hostname information to this handler.
 	the handler will update the session record, so that the Session Initializer
 	can finish its process, and allow the game to start.
+
+	url format:
+		<host>:<port>/subscribe?hostname=<urlencoded hostname>&session_key=<session key"
+
+	eg.
+		http://localhost:9090/subscribe?hostname=http%3A%2F%2Flocalhost%3A9091%2Fevent&session_key=1
 """
 
 import tornado.web
 import tornadis
 
 class BotSubscriptionHandler(tornado.web.RequestHandler):
+	def initialize(self, sync_redis):
+		self.sync_redis = sync_redis
+
 	@tornado.gen.coroutine
 	def get(self):
-
 		hostname = self.get_argument('hostname')
 		session_key = self.get_argument('session_key')
 
-		session_store = tornadis.Client()
-		yield session_store.connect()
-		# NOTE should do some syncronous checks here before setting
-		# so that we can notify the bot if its already been set in the time
-		# that it took to join
-		# reply with error so the bot can keep searching
-		# try dropping in pyredis in here. should hold up the ioloop! :D
-		yield session_store.call('HSET', 'sessions', session_key, hostname)
-		keys = yield session_store.call('HGETALL', 'sessions')
+		# use non-async redis, so that we can't overwrite something that
+		# has just been written, while the callback is waiting for a reply.
+		# not sure if this is possible, but just in case until verified!
+		self.sync_redis.hset('sessions', session_key, hostname)
+		keys = self.sync_redis.hgetall('sessions')
 		print 'hostname set. all keys: ', keys
 
-		self.write("")
 		self.finish()
