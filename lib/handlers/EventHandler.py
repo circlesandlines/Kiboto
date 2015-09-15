cba7dd938363b553b289d28df11534691b4d36e7
Radu Nicolae <ranicolae@gmail.com>
2015-09-15 00:06:30 -0400

    edge case handling for event handler. once this is complete do one more wave of strngthening the cohesion of these error handling methods

--- lib/handlers/EventHandler.py
+++ lib/handlers/EventHandler.py
@@ -14,32 +14,45 @@ class EventHandler(tornado.web.RequestHandler):
 	@tornado.gen.coroutine
 	def post(self):
 		# extract POST event data
-		# TODO handle non-json error
-		event_data = json.loads(self.request.body)
+		try:
+			event_data = json.loads(self.request.body)
+		except Exception as e:
+			raise NotJSONException("don't sent non-json event data")
+
 		session_key = 'session:' + event_data['session_key']
 
-		# TODO handle 'no event object' in message
-		bot_message = json.dumps({'event': event_data['message']})
+		try:
+			bot_message = json.dumps({'event': event_data['message']})
+		except KeyError:
+			raise NoEventMessageException("no 'message' was sent")
 
 		# check if session exists in local store
 		if session_key not in self.local_sessions.sessions:
 			# cache miss. go to redis
 			session_store = tornadis.Client()
 
-			# TODO handle connection error here
 			yield session_store.connect()
 			hostname = yield session_store.call('HGET', 'sessions', session_key)
 
 			if not hostname:
-				# TODO this should return an error to game
-				# does it return a 500? should be 404, no session found
+				reply = {
+					'error': 1,
+					'err_msg': "server-side session store error"
+				}
+				self.write(json.dumps(reply))
+				self.finish()
 				raise NoSessionInStoreException("session store does not contain a bot for session: " + session_key)
 		else:
 			try:
 				hostname = self.local_sessions.sessions[session_key]
 			except:
-				# TODO this should return an error to game
-				# does it return a 500? should be 404, no session found
+				reply = {
+					'error': 1,
+					'err_msg': "server-side session cache error"
+				}
+				self.write(json.dumps(reply))
+				self.finish()
+
 				raise NoSessionInStoreException("session cache does not contain a bot for session: " + session_key)
 
 		print "bot url: ", hostname
@@ -52,13 +65,21 @@ class EventHandler(tornado.web.RequestHandler):
 							request_timeout=1 )
 
 		# TODO handle connection error
-		http_client = httpclient.AsyncHTTPClient()
-		action = yield http_client.fetch(request)
+		try:
+			http_client = httpclient.AsyncHTTPClient()
+			action = yield http_client.fetch(request)
+		except Exception as e:
+			# this seems lazy, but really, no errors should be happening here
+			print 'bot action error: ', e
+			reply = {
+				'error': 1,
+				'err_msg': "no action received. bot error"
+			}
+			self.write(json.dumps(reply))
+			self.finish()
+			return
 
-		# TODO check the action response for bad format
-		# ie. just make sure its json
-
-		print action.body
+		print 'action: ', action.body
 
 		self.write(json.dumps(action.body))
 		self.finish()
